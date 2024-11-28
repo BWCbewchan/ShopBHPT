910343ff6f8f0d955169187fe2640571dcdccf69
BWCbewchan <bwc.bewchan@gmail.com>
2024-11-29 00:14:25 +0700

    them id_item của commnet

--- src/main/java/com/shop/bhpt/entities/Comment.java
+++ src/main/java/com/shop/bhpt/entities/Comment.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,4 +24,8 @@ public class Comment {
     private String userName;
     private int star;
     private String content;
+    
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
 }
